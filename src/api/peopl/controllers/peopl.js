0c3c0357b28ff4815da52dd6debcd9bf2159f201
Islam Temirgaliev <islam2004tr@mail.ru>
2023-03-31 23:21:53 +0600

    commit people

---
+++ src/api/peopl/controllers/peopl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * peopl controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::peopl.peopl');
---
+++ src/api/peopl/routes/peopl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * peopl router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::peopl.peopl');
